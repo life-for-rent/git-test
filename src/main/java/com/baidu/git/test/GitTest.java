4cc0011873ec0ad6d883b2519952e22621ce1910
gong <gong@qq.com>
2021-09-28 18:19:44 +0800

    commit2

--- src/main/java/com/baidu/git/test/GitTest.java
+++ src/main/java/com/baidu/git/test/GitTest.java
@@ -8,5 +8,6 @@ package com.baidu.git.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git");
+        System.out.println("hello Git22");
     }
 }
