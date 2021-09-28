d0af2dd60140d9060173579f9dffa9d8a7879488
gong <gong@qq.com>
2021-09-28 16:56:31 +0800

    first commit

---
+++ src/main/java/com/baidu/git/test/GitTest.java
@@ -0,0 +1,12 @@
+package com.baidu.git.test;
+
+/**
+ * @author: gong
+ * @date: 2021/9/28
+ * @description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+}
